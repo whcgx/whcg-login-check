39a44edf4958e96be4c3eebe381e3a05f0196825
Andersh75 <andersh75@gmail.com>
2018-06-15 12:58:58 +0200

    first commit

---
+++ dist/index.es6.js
@@ -0,0 +1,90 @@
+import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+import '@polymer/polymer/lib/elements/dom-if.js';
+
+/**
+ * LoginCheck
+ */
+class LoginCheck extends PolymerElement {
+
+  static get properties() {
+    return {
+      user: {
+        type: Object,
+        notify: true,
+        readOnly: false,
+        observer: '_userChanged'
+      },
+      path: {
+        type: String,
+        notify: true,
+        readOnly: false,
+        observer: '_pathChanged'
+      },
+      tail: {
+        type: Object,
+        notify: true,
+        readOnly: false
+      }
+    };
+  }
+
+  constructor() {
+    super();
+  }
+
+  _pathChanged(newValue, oldValue) {
+    if (this.user) {
+      if(this.path === 'user' || 'start') {
+        // console.log('this.tail');
+        // console.log(this.tail.path);
+        // if(this.tail.path === '/fallback') {
+        //   this.tail.path = "/";
+        // }
+        window.location = '/#/user' + this.tail.path;
+      } else {
+        window.location = '/#/user';
+      } 
+    } else {
+      if(this.path === 'user' || 'start') {
+        // console.log('this.tail');
+        // console.log(this.tail.path);
+        // if(this.tail.path === '/fallback') {
+        //   this.tail.path = "/";
+        // }
+        window.location = '/#/start' + this.tail.path;
+      } else {
+        window.location = '/#/start';
+      } 
+    }
+  }
+
+  _userChanged(newValue, oldValue) {
+    if (this.user) {
+      if(this.path === 'user' || 'start') {
+        // console.log('this.tail');
+        // console.log(this.tail.path);
+        // if(this.tail.path === '/fallback') {
+        //   this.tail.path = "/";
+        // }
+        window.location = '/#/user' + this.tail.path;
+      } else {
+        window.location = '/#/user';
+      } 
+    } else {
+      if(this.path === 'user' || 'start') {
+        // console.log('this.tail');
+        // console.log(this.tail.path);
+        // if(this.tail.path === '/fallback') {
+        //   this.tail.path = "/";
+        // }
+        window.location = '/#/start' + this.tail.path;
+      } else {
+        window.location = '/#/start';
+      } 
+    }
+  }
+}
+
+window.customElements.define('login-check', LoginCheck);
+
+export { LoginCheck };
---
+++ readme.md
@@ -0,0 +1,14 @@
+# &lt;login-check&gt;
+
+```html
+<login-check user="{{user}}"></login-check>
+```
+
+## Getting started 
+
+Checks if the property *user* is truish and selects template accordingly.
+
+## Dependencies
+
+&lt;login-check&gt; is a Polymer 3 element with the following dependencies: 
+- npm i @polymer/polymer
